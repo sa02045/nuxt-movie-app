f688bfede97658c1b1089d70e5909ea7828359c8
sa02045 <sa02045@naver.com>
2022-05-19 08:20:27 +0900

    feat: 영화 키워드검색 페이지 추가

--- pages/movies/keyword/_keyword.vue
+++ pages/movies/keyword/_keyword.vue
@@ -1,20 +1,35 @@
 <template>
-  <div class="container">
-    <CBox d="flex" w="100vw" h="100vh" flex-dir="column">
-      <CHeading text-align="center" mb="4"> keyword 검색기 </CHeading>
-      <CFlex justify="center" align="center">
-        <CInput
-          placeholder="영화를 검색하세요"
-          size="md"
-          w="50vw"
-          v-model="keyword"
-        />
-        <NuxtLink to="/movies/">
-          <CButton left-icon=""> 검색 </CButton>
-        </NuxtLink>
-      </CFlex>
-    </CBox>
-  </div>
+  <CFlex d="flex" w="100vw" h="100vh" flex-dir="column" align="center">
+    <CHeading text-align="center"> 영화 검색기 </CHeading>
+    <CFlex>
+      <CInput
+        placeholder="영화를 검색하세요"
+        size="md"
+        w="50vw"
+        v-model="keyword"
+      />
+      <NuxtLink :to="`/movies/keyword/${keyword}`">
+        <CButton left-icon=""> 검색 </CButton>
+      </NuxtLink>
+    </CFlex>
+    <c-grid
+      padding="10"
+      mt="40px"
+      w="100%"
+      template-columns="repeat(4, 1fr)"
+      gap="6"
+    >
+      <c-grid-item v-for="movie in movies" :key="movie.imdbID">
+        <movie-card
+          :key="movie.imdbID"
+          :title="movie.Title"
+          :imdbID="movie.imdbID"
+          :year="movie.Year"
+          :poster="movie.Poster"
+        ></movie-card
+      ></c-grid-item>
+    </c-grid>
+  </CFlex>
 </template>
 
 <script lang="js">
@@ -23,8 +38,12 @@ import {
   CButton,
   CFlex,
   CHeading,
-  CInput
+  CInput,
+  CGrid,
+  CGridItem
 } from '@chakra-ui/vue'
+import { fetchMovieList } from '../../../api/api'
+import MovieCard from '../../../components/MovieCard.vue'
 
 export default {
   name: 'IndexPage',
@@ -33,19 +52,27 @@ export default {
     CButton,
     CFlex,
     CHeading,
-    CInput
+    CInput,
+    MovieCard,
+    CGrid,
+    CGridItem
+  },
+  asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
+      return{
+          keyword:params.keyword
+      }
   },
   data () {
     return {
-      keyword:""
+      keyword:"",
+      movies:[],
     }
   },
   computed: {
   },
-  methods: {
-    onClick(){
-
-    }
-  }
+  async fetch(){
+      const res  = await fetchMovieList(this.keyword)
+      this.movies = res.Search
+  },
 }
 </script>
